71c811af10b80ce646ee7deef0d7dbed4f994a8a
root0fall <root0fall@nullcorp.com>
2023-03-20 21:54:35 +0800

    switch to pillow

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # kneeboard-extractor
-extract BMS kneeboard pages
+
+usage: Kneeboard Extractor [-h] [--monitor] [--basedir BASEDIR] --left LEFT
+                           [--right RIGHT] [--debug] [--silent]
+
+Extract Kneeboard images from BMS files
+
+options:
+  -h, --help         show this help message and exit
+  --monitor          automatically detect changes to BMS kneeboard files, and
+                     write to output directory
+  --basedir BASEDIR  BMS base directory for locating kneeboard files
+  --left LEFT        directory to output left kneeboard images
+  --right RIGHT      directory to output right kneeboard images (optional)
+  --debug            enable debug messages
+  --silent           disable messages
--- kneeboard_extractor.py
+++ kneeboard_extractor.py
@@ -5,14 +5,17 @@ import os
 import time
 import winreg
 import argparse
-from wand.image import Image
+from PIL import Image
+
+VERSION = "0.9.0"
 
 INIT_DDS = 7982
 DDS_COUNT = 16
 
 DEBUG = False
-BMS_VERSION = '4.37'
-WIN_REG_KEY = rf'SOFTWARE\WOW6432Node\Benchmark Sims\Falcon BMS {BMS_VERSION}'
+INFO = True
+BMS_VERSION = "4.37"
+WIN_REG_KEY = rf"SOFTWARE\WOW6432Node\Benchmark Sims\Falcon BMS {BMS_VERSION}"
 DDS_DIR = os.path.join("Data","TerrData","Objects","KoreaObj")
 EXT = ".dds"
 OUT_EXT = "png"
@@ -23,6 +26,11 @@ def debug(message):
     '''debug message wrapper'''
     if DEBUG:
         sys.stderr.write(f"\n{message}\n")
+
+def info(message):
+    '''print message'''
+    if INFO:
+        print(message)
 
 def get_dds_dir(base_dir):
     '''path to the BMS DDS files'''
@@ -40,7 +48,7 @@ def get_base_dir(base_dir):
             for element in range(8):
                 try:
                     vals = winreg.EnumValue(key, element)
-                    if vals[0] == 'baseDir':
+                    if vals[0] == "baseDir":
                         ret_base = os.path.normpath(vals[1])
                         if os.path.isdir(ret_base):
                             return ret_base
@@ -58,17 +66,20 @@ def write_new_files(base_dir, leftdir, rightdir):
     '''write new kneeboard images to output directory'''
 
     for i in range(INIT_DDS, INIT_DDS + DDS_COUNT):
-        with Image(filename=os.path.join(get_dds_dir(base_dir), str(i) + EXT)) as img:
-            with img[0:int(img.size[0]/2), 0:img.size[1]] as cropped:
-                cropped.format = OUT_EXT
-                lfname = os.path.join(leftdir, f"L_{str(i)}.{OUT_EXT}")
-                print(f"writing left knee: {lfname}")
-                cropped.save(filename=lfname)
-            with img[int(img.size[0]/2):img.size[0], 0:img.size[1]] as cropped2:
-                cropped2.format = OUT_EXT
-                rfname = os.path.join(rightdir or leftdir, f"R_{str(i)}.{OUT_EXT}")
-                print(f"writing right knee: {rfname}")
-                cropped2.save(filename=rfname)
+        original = os.path.join(get_dds_dir(base_dir), str(i) + EXT)
+        with Image.open(original) as img:
+
+            left_dims = (0,0,int(img.size[0]/2),img.size[1])
+            cropped = img.crop(left_dims)
+            lfname = os.path.join(leftdir, f"L_{str(i)}.{OUT_EXT}")
+            info(f"writing left knee: {lfname}")
+            cropped.save(lfname, format=OUT_EXT)
+
+            right_dims = int(img.size[0]/2),0,img.size[0],img.size[1]
+            cropped2 = img.crop(right_dims)
+            rfname = os.path.join(rightdir or leftdir, f"R_{str(i)}.{OUT_EXT}")
+            info(f"writing right knee: {rfname}")
+            cropped2.save(rfname, format=OUT_EXT)
 
 def monitor_sequence():
     '''generator for monitor message'''
@@ -87,40 +98,50 @@ def monitor(base_dir, *dirs):
     '''monitor for changes to kneebaord files'''
 
     check_file = os.path.join(get_dds_dir(base_dir), str(INIT_DDS) + EXT)
+    check_file_last = os.path.join(get_dds_dir(base_dir), str(INIT_DDS + DDS_COUNT - 1) + EXT)
     prev_time = get_time(check_file)
+    prevl_time = get_time(check_file_last)
     for msg in monitor_sequence():
-        sys.stdout.write('\r' + msg)
+        if INFO:
+            sys.stdout.write("\r" + msg)
         new_time = get_time(check_file)
         if prev_time != new_time:
-            print("change detected writing fresh files")
-            time.sleep(10)
+            info("\rchange detected waiting for modification completion")
+            newl_time = get_time(check_file_last)
+            while newl_time == prevl_time:
+                newl_time = get_time(check_file_last)
+                time.sleep(1)
             write_new_files(base_dir, *dirs)
             prev_time = new_time
-            print("fresh files written")
+            info("fresh files written")
         time.sleep(0.2)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="Kneeboard Extractor",
                                     description="Extract Kneeboard images from BMS files")
-    parser.add_argument("--monitor", action='store_true', help="automatically detect changes to " +\
+    parser.add_argument("--monitor", action="store_true", help="automatically detect changes to " +\
                         "BMS kneeboard files, and write to output directory")
     parser.add_argument("--basedir", help="BMS base directory for locating kneeboard files")
     parser.add_argument("--left", required=True, help="directory to output left kneeboard images")
     parser.add_argument("--right", required=False,
-                        help="directory to output right kneeboard images")
-    parser.add_argument("--debug", action='store_true', help="enable debug messages")
+                        help="directory to output right kneeboard images (optional)")
+    parser.add_argument("--debug", action="store_true", help="enable debug messages")
+    parser.add_argument("--silent", action="store_false", help="disable messages")
 
     args = parser.parse_args()
 
     DEBUG = args.debug if args.debug is not None else DEBUG
+    INFO = args.silent if args.silent is not None else INFO
 
     basedir = get_base_dir(args.basedir)
     try:
         if args.monitor:
-            monitor(basedir, args.left, args.right)
+            monitor(basedir, args.left.strip("\\"),
+                    args.right.strip("\\") if args.right else None)
         else:
-            write_new_files(basedir, args.left, args.right)
+            write_new_files(basedir, args.left.strip("\\"),
+                            args.right.strip("\\") if args.right else None)
     except KeyboardInterrupt:
-        sys.stderr.write("Exiting...\n")
+        sys.stderr.write("\rExiting...\n")
         sys.exit(0)
